9b27bc6eddaf036094634918174fdf60746f5560
sergey <bikeshop.dnipro@gmail.com>
2024-04-15 20:46:01 +0300

    Howework 16

--- 15_lesson/15_lesson.js
+++ 15_lesson/15_lesson.js
@@ -1,76 +1,76 @@
 'use strict';
 
-// Задача 1-1
-function showDelay(msg) {
-    setTimeout(() => {
-        console.log(msg);
-    }, 2000);
-}
-
-showDelay('1st exercise');
-
-
-// Задача 1-2
-function repeat(msg) {
-    setInterval(() => {
-        console.log(msg);
-    }, 2000);
-}
-
-repeat('2d exercise');
-
-// Задача 1-3
-function repeatRecursive(msg) {
-    let counter = 0;
-    let printMsgId = setTimeout(function repeat() {
-        console.log(`${msg} ${++counter}`);
-        printMsgId = setTimeout(repeat, 2000);
-    }, 2000);
-}
-
-repeatRecursive('message #')
-
-
-
-// Задача 1-4
-function boom(count) {
-    let boomId = setInterval(() => {
-        if (count === 0) {
-            clearInterval(boomId);
-            console.log('BOOM!!!');
-            return;
-        } else if (count < 0) {
-            console.log(`Count: ${count++}`);
-        } else {
-            console.log(`Count: ${count--}`);
-        }
-    }, 1000);
-}
-
-boom(-10);
-boom(10);
-
-
-
-// Задача 2
-function sayHi() {
-    console.log('Hello');
-}
-
-function delay(fn, sec) {
-    function delayedFunc() {
-        setTimeout(fn, sec * 1000);
-    }
-    return delayedFunc;
-}
-
-const testDelay = delay(sayHi, 5);
-
-sayHi();
-testDelay();
-
-
-
+// // Задача 1-1
+// function showDelay(msg) {
+//     setTimeout(() => {
+//         console.log(msg);
+//     }, 2000);
+// }
+//
+// showDelay('1st exercise');
+//
+//
+// // Задача 1-2
+// function repeat(msg) {
+//     setInterval(() => {
+//         console.log(msg);
+//     }, 2000);
+// }
+//
+// repeat('2d exercise');
+//
+// // Задача 1-3
+// function repeatRecursive(msg) {
+//     let counter = 0;
+//     let printMsgId = setTimeout(function repeat() {
+//         console.log(`${msg} ${++counter}`);
+//         printMsgId = setTimeout(repeat, 2000);
+//     }, 2000);
+// }
+//
+// repeatRecursive('message #')
+//
+//
+//
+// // Задача 1-4
+// function boom(count) {
+//     let boomId = setInterval(() => {
+//         if (count === 0) {
+//             clearInterval(boomId);
+//             console.log('BOOM!!!');
+//             return;
+//         } else if (count < 0) {
+//             console.log(`Count: ${count++}`);
+//         } else {
+//             console.log(`Count: ${count--}`);
+//         }
+//     }, 1000);
+// }
+//
+// boom(-10);
+// boom(10);
+//
+//
+//
+// // Задача 2
+// function sayHi() {
+//     console.log('Hello');
+// }
+//
+// function delay(fn, sec) {
+//     function delayedFunc() {
+//         setTimeout(fn, sec * 1000);
+//     }
+//     return delayedFunc;
+// }
+//
+// const testDelay = delay(sayHi, 5);
+//
+// sayHi();
+// testDelay();
+//
+//
+//
 // Задача 3
 class Timer {
     #objIntervalId;
@@ -92,11 +92,11 @@ class Timer {
 
     pause() {
         clearInterval(this.#objIntervalId);
-        // так як "пауза" вмикається на 5-ій секунді, то цифра "5" не відібражається в консолі "старта". 
-        // Тому її вивів окремо. 
+        // так як "пауза" вмикається на 5-ій секунді, то цифра "5" не відібражається в консолі "старта".
+        // // Тому її вивів окремо.
         console.log(++this.timer);
-        this.#objIntervalId = setInterval(() => {
-        }, 1000);
+        // this.#objIntervalId = setInterval(() => {
+        // }, 1000);
     }
 
     show() {
@@ -123,42 +123,42 @@ setTimeout(() => {
 
 
 
-// Задача 4
-// Я зробив припущення, що страви для клиєнтів готуються разом одночасно.
-function makeListOfDishes(name, obj) {
-    let tempArray = [name];
-    let maxTime = 0;
-    for (let element of obj) {
-        tempArray.push(element.name);
-        maxTime = (element.time > maxTime) ? element.time : maxTime;
-    }
-    tempArray.push(maxTime);
-    return tempArray;
-}
-
-const customers = {
-    "Katy Perry": [
-        {name: "Borsh", time: 5000},
-        {name: "vareniki", time: 1000},
-        {name: "uzvar", time: 500},
-    ],
-    "Cristiano Ronaldo": [
-        {name: "Soup", time: 7000},
-        {name: "porridge", time: 1000},
-        {name: "water", time: 100},
-    ],
-    "Jason Statham": [
-        {name: "fried potatoes", time: 6000},
-        {name: "steak", time: 10000},
-        {name: "juice", time: 100},
-    ],
-};
-
-for (let customer in customers) {
-    let newArray = makeListOfDishes(customer, customers[customer]);
-    let name = newArray.shift();
-    let time = newArray.pop();
-    setTimeout(() => {
-        console.log(`Шановний відвідувач, ${name}, ваше замовлення готове: ${newArray.join(', ')}. Час приготування ${time / 1000} минут. Смачного!`);
-    }, time);
-}
+// // Задача 4
+// // Я зробив припущення, що страви для клиєнтів готуються разом одночасно.
+// function makeListOfDishes(name, obj) {
+//     let tempArray = [name];
+//     let maxTime = 0;
+//     for (let element of obj) {
+//         tempArray.push(element.name);
+//         maxTime = (element.time > maxTime) ? element.time : maxTime;
+//     }
+//     tempArray.push(maxTime);
+//     return tempArray;
+// }
+//
+// const customers = {
+//     "Katy Perry": [
+//         {name: "Borsh", time: 5000},
+//         {name: "vareniki", time: 1000},
+//         {name: "uzvar", time: 500},
+//     ],
+//     "Cristiano Ronaldo": [
+//         {name: "Soup", time: 7000},
+//         {name: "porridge", time: 1000},
+//         {name: "water", time: 100},
+//     ],
+//     "Jason Statham": [
+//         {name: "fried potatoes", time: 6000},
+//         {name: "steak", time: 10000},
+//         {name: "juice", time: 100},
+//     ],
+// };
+//
+// for (let customer in customers) {
+//     let newArray = makeListOfDishes(customer, customers[customer]);
+//     let name = newArray.shift();
+//     let time = newArray.pop();
+//     setTimeout(() => {
+//         console.log(`Шановний відвідувач, ${name}, ваше замовлення готове: ${newArray.join(', ')}. Час приготування ${time / 1000} минут. Смачного!`);
+//     }, time);
+// }
---
+++ 16_lesson/16_lesson.js
@@ -0,0 +1,141 @@
+'use strict';
+
+
+// Задача 1-1
+const promise1 = new Promise((resolve, reject) => {
+    resolve('Hello world');
+});
+
+promise1.then((value) => {
+    console.log(value);
+});
+
+
+Promise.resolve().then(() => {console.log("Hello World")});
+
+
+// Задача 1-2
+function delayedMsg(msg) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve(msg);
+        }, 2000);
+    }).then((result) => {
+        console.log(result)
+    });
+}
+
+delayedMsg('Hello world');
+
+
+// Задача 1-3
+const errorPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        const myError = new Error('My error');
+        reject(myError);
+    });
+});
+
+errorPromise.catch(console.log);
+
+
+// Задача 1-4
+function makePromise(msg, delay) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve(msg);
+        }, delay)
+    });
+}
+function showResults(...args) {
+    let promisesResults = Promise.allSettled(args);
+    promisesResults.then((results) => {
+        results.forEach((result) => {
+            console.log(result.value);
+        })
+    })
+}
+
+let prom1 = makePromise('Promise 1', 1000);
+let prom2 = makePromise('Promise 2', 100);
+let prom3 = makePromise('Promise 3', 500);
+
+showResults(prom1, prom2, prom3);
+
+
+// Задача 1-5
+function showFirst(...args) {
+    Promise.any(args).then(console.log);
+}
+
+showFirst(prom1, prom2, prom3);
+
+
+// Задача 2
+function delay(ms) {
+    const myPromise = new Promise(((resolve, reject) => {
+        setTimeout(() => {
+            resolve(ms);
+            console.log(myPromise);
+        }, ms);
+    }));
+    console.log(myPromise);
+    return myPromise;
+}
+
+delay(5000)
+    .then((time) => alert(`Виконалось через ${time / 1000} секунд`))
+    .catch(console.error);
+
+
+// Задача 3
+function callbackFunction(error, result) {
+    if (error !== null) return error;
+    return `результат операції ${result}`
+}
+
+function callback_BasedFunction(arg1, arg2, callback) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let result = arg1 + arg2;
+            // Імітуємо важку операцію
+            for (let i = 0; i < 1000000000; i++) {}
+            if (result % 2 !== 0) {
+                resolve(callback(null, result));
+            }
+            reject(callback(new Error('Result is not odd!'), null));
+        })
+    })
+}
+
+console.log("start");
+
+callback_BasedFunction(3150, 121, callbackFunction)
+    .then(console.log)
+    .catch(console.error);
+
+console.log("end");
+
+
+// Задача 4
+const cache = {};
+function getFibonachiRecursion(number) {
+    if (number <= 2) return 1;
+    cache[number] = cache[number] || getFibonachiRecursion(number - 1) + getFibonachiRecursion(number - 2);
+    return cache[number];
+}
+
+function fibonachiAsync(number) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            let result = getFibonachiRecursion(number);
+            resolve(result);
+        });
+    })
+}
+
+console.log("start");
+
+fibonachiAsync(40).then(console.log);
+
+console.log("finish");
